72fb322431fcc4e1ecda4d7a1d48a1fe8daf6f6b
Saugat-Shrestha <Shresthaasaugat31@gmail.com>
2023-11-01 08:37:36 +0545

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Calculator());
+}
+
+class Calculator extends StatelessWidget {
+  const Calculator({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
